f4819cad896cc97143fa06c7be305d2f3a4aab6a
DRWilliams527 <94419369+DRWilliams527@users.noreply.github.com>
2022-03-15 14:56:33 -0700

    Revert "linked db and setup redux"

--- client/main.js
+++ client/main.js
@@ -1,12 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { store } from './store.js'
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <Provider store = {store}>
-        <h1>Hello World</h1>
-    </Provider>,
+    <h1>Hello World</h1>,
     document.getElementById('root')
 );
