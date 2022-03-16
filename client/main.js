354bcf4759383b847e8840c3b6ed176f33a8342c
drwilliams527 <drakeocinco@gmail.com>
2022-03-15 20:02:12 -0700

    actions set up in reducer

--- client/main.js
+++ client/main.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
+
 
 ReactDOM.render(
-    <h1>Hello World</h1>,
+    <Provider store = {store}>
+        <h1>Hello World</h1>
+    </Provider>,
     document.getElementById('root')
 );
